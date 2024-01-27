2d7cd0c2c1668f64614bcfb90052c9560e8d8204
Ethan Long <etttthhhhaaaan@gmail.com>
2024-01-27 15:37:24 -0500

    fixed the aboutme tab

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
-import { Circle } from "./Circle";
-import { RainbowHighlight } from "./RainbowHighlight";
-import { Underline } from "./Underline";
-import { Box } from "./Box";
-import { Bracket } from "./Bracket";
 
-import userData from "@constants/data";
+
+
 
 export default function AboutMe() {
   const colors = ["#088f8f", "#84CC16", "#10B981", "#3B82F6", "#FF0000"];
